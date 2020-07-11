81717c837b9f849f7d0981faa555884c97abf49d
mrToshkin <toshkin.na@gmail.com>
2020-07-11 17:04:20 +0400

    [8] Komponentniy podhod

--- js/dialog-avatar.js
+++ js/dialog-avatar.js
@@ -1,14 +1,20 @@
 'use strict';
 
 (function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
   var fileChooser = document.querySelector('.upload input[type=file]');
   var preview = window.utils.setup.querySelector('.setup-user-pic');
   var setupOpenIcon = document.querySelector('.setup-open-icon');
 
   fileChooser.addEventListener('change', function () {
     var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+    
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
 
-    if (file) {
+    if (matches) {
       var reader = new FileReader();
 
       reader.addEventListener('load', function () {
